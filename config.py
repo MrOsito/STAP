1ba293c581aee3c904993408f4569c72bc7e55cf
Gerard Corboz <gcorboz@gmail.com>
2025-05-05 10:13:02 +1000

    Resolved silly bug by importing os
     Changes to be committed:
    	modified:   config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 CLIENT_ID = os.environ.get("COGNITO_CLIENT_ID", "6v98tbc09aqfvh52fml3usas3c")
 AWS_REGION = os.environ.get("AWS_REGION", "ap-southeast-2")
 MEMBERS_URL = "https://members.terrain.scouts.com.au"
