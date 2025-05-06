4b0fdb44fc2856cce730e1e6a3cd91956c3b4519
Gerard Corboz <gcorboz@gmail.com>
2025-05-06 13:48:24 +1000

    Mucking around with Debug timing
     Changes to be committed:
    	modified:   static/calendar.js

--- static/calendar.js
+++ static/calendar.js
@@ -452,12 +452,7 @@ function setupEditEventButton() {
 
         console.time("fetchEvent");
         console.time("fetch /event/<eventId>");
-<<<<<<< HEAD
         return fetch(`/event/${currentEventId}`);
-        console.timeEnd("fetch /event/<eventId>");
-=======
-        return fetch(`/event/${currentEventId}`);        
->>>>>>> 2b5384b (Removed some log debugging and added some)
       })
       console.timeEnd("fetch /event/<eventId>");
       .then(res => {
