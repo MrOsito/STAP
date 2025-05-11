d35de1fd3e6eed9ec0feb63737c318074d72b2ea
MrOsito <gcorboz@gmail.com>
2025-05-11 21:13:50 +1000

    Revert "Instead of BOTO3 we're going to use amplify, a javscript library for AWS, to login via the browser and javascript"
    This reverts commit 2adaed0b470f5b14bab7dd2a39ac43159b7f4773.

--- config.py
+++ config.py
@@ -2,6 +2,5 @@ import os
 
 CLIENT_ID = os.environ.get("COGNITO_CLIENT_ID", "6v98tbc09aqfvh52fml3usas3c")
 AWS_REGION = os.environ.get("AWS_REGION", "ap-southeast-2")
-AWS_USER_POOL_ID = os.environ.get("COGNITO_USER_POOL_ID", "ap-southeast-2_hczmFu5Rv")
 MEMBERS_URL = "https://members.terrain.scouts.com.au"
 EVENTS_API_URL = "https://events.terrain.scouts.com.au"
--- static/config.js
+++
@@ -1,6 +0,0 @@
-// static/config.js
-window.cognitoConfig = {
-  region: "ap-southeast-2",
-  clientId: "6v98tbc09aqfvh52fml3usas3c",      // CLIENT_ID
-  userPoolId: "ap-southeast-2_ABC123",         // AWS_USER_POOL_ID
-};
