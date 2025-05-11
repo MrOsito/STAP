eb5d0864239ac45b047af5bb07647b9ce9b65195
MrOsito <gcorboz@gmail.com>
2025-05-11 19:59:16 +1000

    Add Print functions to identify what's slowing down dashboard
     Changes to be committed:
    	modified:   routes/auth_routes.py

--- routes/auth_routes.py
+++ routes/auth_routes.py
@@ -1,11 +1,13 @@
 # routes/auth_routes.py
-from flask import Blueprint, session, redirect, url_for, flash, request, render_template
+from flask import Blueprint, session, redirect, url_for, flash, render_template
 from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import StringField, PasswordField, SelectField
 from wtforms.validators import DataRequired, Regexp
 from config import AWS_REGION, CLIENT_ID
 from services.api_helpers import get_profiles
 from markupsafe import escape
+import time
+
 
 # Initialize CSRF protection (in your app factory or app.py)
 csrf = CSRFProtect()
@@ -38,13 +40,14 @@ def logout():
 @auth_bp.route('/', methods=['GET', 'POST'])
 @auth_bp.route('/login', methods=['GET', 'POST'])
 def login_route():
+    start_time = time.time()
     form = LoginForm()
     if form.validate_on_submit():
         # Escape sanitized inputs
         branch    = escape(form.branch.data)
         username  = escape(form.username.data)
         password  = form.password.data  # password isn't echoed back
-
+        print(f"⏱️ Just prior to login {time.time() - start_time:.2f} seconds", flush=True)
         try:
             import boto3
             aws = boto3.client('cognito-idp', region_name=AWS_REGION)
@@ -57,11 +60,13 @@ def login_route():
                 }
             )
             id_token = resp['AuthenticationResult']['IdToken']
-
+            print(f"⏱️ After login {time.time() - start_time:.2f} seconds and fetch of id_token {id_token}", flush=True)
             # fetch and store profile
-            profile_data = get_profiles(id_token)
-            profile = profile_data.get('profiles', [{}])[0]
 
+            profile_data = get_profiles(id_token)
+            print(f"⏱️ After get_profiles {time.time() - start_time:.2f} seconds and fetch of id_token {id_token}", flush=True)
+
+            profile = profile_data.get('profiles', [{}])[0]
             session['user'] = {
                 'username': f'{branch}{username}',
                 'id_token': id_token,
@@ -73,6 +78,8 @@ def login_route():
                 'group_name': profile.get('group', {}).get('name'),
             }
             session.permanent = True
+            print(f"⏱️ After populating session user {time.time() - start_time:.2f} seconds and fetch of id_token {id_token}", flush=True)
+
             return redirect(url_for('dashboard.dashboard'))
 
         except Exception as e:
