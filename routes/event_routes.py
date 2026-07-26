dc449b63e6bbc35be44d9b8d4500012feb8d52a3
MrOsito <gcorboz@gmail.com>
2026-07-26 22:26:46 +1000

    instead of returning a Success True JSON I am redirectig back to the calendar so as to avoid rendering a blank page

--- routes/event_routes.py
+++ routes/event_routes.py
@@ -51,7 +51,8 @@ def create_event():
         with httpx.Client(timeout=10.0) as client:
             res = client.post(url, headers=headers, json=event_data)
             res.raise_for_status()
-            return jsonify({"success": True})
+#            return jsonify({"success": True})
+            return redirect(url_for('dashboard.calendar'))
     except httpx.HTTPError as e:
         print(f"[ERROR] Creating event: {e}")
         return api_error(str(e))
